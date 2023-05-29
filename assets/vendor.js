ff7747b6f8a29fe115085e27dde0139a2bca78c4
TetraTheta <37061801+TetraTheta@users.noreply.github.com>
2023-05-30 01:58:52 +0900

    fix: Apply Fancybox correctly

--- assets/vendor.js
+++ assets/vendor.js
@@ -23,21 +23,7 @@ const swiper = new Swiper('.swiper', {
   }
 })
 
-window.swiper = swiper;
+window.swiper = swiper
 
-/* Replace Lightbox using Fancybox */
-(function () {
-  HTMLCollection.prototype.forEach = Array.prototype.forEach
-  const s = document.getElementsByTagName('span')
-  s.forEach((e) => {
-    if ('lightbox' in e.dataset) {
-      delete e.dataset.lightbox
-      e.dataset.fancybox = 'fancybox'
-      e.dataset.src = e.getElementsByTagName('img')[0].dataset.src
-    }
-  })
-})()
-
-/** @see https://fancyapps.com/fancybox */
 import { Fancybox } from '@fancyapps/ui'
 Fancybox.bind('[data-fancybox]')
